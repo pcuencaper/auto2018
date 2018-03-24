69b6d96cd8afab2dee26ad2016c5bb3ba7193a97
Clase <Clase@1A-PC10>
2018-03-24 13:38:24 -0500

    primer comentario gib

---
+++ src/main/java/com/autokm/autokm/AutokmApplication.java
@@ -0,0 +1,12 @@
+package com.autokm.autokm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutokmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutokmApplication.class, args);
+	}
+}
